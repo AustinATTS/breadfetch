3d81766f3f5ea478b3c2aa506a3b11d138427962
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-02-08 22:50:44 +0000

    Setup the display structure

---
+++ include/display.h
@@ -0,0 +1,24 @@
+#pragma once
+#define DISPLAY_HEIGHT
+
+#include <string>
+#include <vector>
+
+namespace breadfetch {
+    class Display {
+    public:
+        static void PrintBanner ();
+        static void PrintInfo (const std::string& label, const std::string& value);
+        static void PrintSeperator ();
+
+    private:
+        static const char* RESET;
+        static const char* BOLD;
+        static const char* RED;
+        static const char* GREEN;
+        static const char* YELLOW;
+        static const char* BLUE;
+        static const char* MAGENTA;
+        static const char* CYAN;
+    };
+} // namespace breadfetch
