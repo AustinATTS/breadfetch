9b67282cfd9798f2c7d207766980158b2b60624e
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-02-08 22:54:44 +0000

    Setup the system_info structure

---
+++ include/system_info.h
@@ -0,0 +1,19 @@
+#pragma once
+#define SYSTEM_INFO_HEADER
+
+#include <string>
+
+namespace breadfetch {
+    class SystemInfo {
+        public:
+        static std::string GetHostname ();
+        static std::string GetUsername ();
+        static std::string GetOS ();
+        static std::string GetKernel ();
+        static std::string GetUptime ();
+        static std::string GetShell ();
+        static std::string GetCPU ();
+        static std::string GetMemory ();
+        static std::string GetArchitecture ();
+    };
+}
