926716d6f83033cc032a29d1c33913ff07804ab3
Austin Welsh-Graham <admin@toastysoftware.co.uk>
2026-02-09 13:57:36 +0000

    Added all the other ansi codes just in case

--- include/display.h
+++ include/display.h
@@ -13,11 +13,31 @@ namespace breadfetch {
     private:
         static const char* RESET;
         static const char* BOLD;
+        static const char* ITALIC;
+        static const char* UNDERLINE;
+        static const char* BLINK;
+        static const char* VIDEO;
+        static const char* CONCEAL;
+        static const char* CROSSED;
+
+        static const char* BLACK;
         static const char* RED;
         static const char* GREEN;
         static const char* YELLOW;
         static const char* BLUE;
         static const char* MAGENTA;
         static const char* CYAN;
+        static const char* WHITE;
+        static const char* DEFAULT;
+
+        static const char* BBLACK;
+        static const char* BRED;
+        static const char* BGREEN;
+        static const char* BYELLOW;
+        static const char* BBLUE;
+        static const char* BMAGENTA;
+        static const char* BCYAN;
+        static const char* BWHITE;
+        static const char* BDEFAULT;
     };
 } // namespace breadfetch
--- src/display.cc
+++ src/display.cc
@@ -5,12 +5,32 @@
 namespace breadfetch {
     const char* Display::RESET = "\033[0m";
     const char* Display::BOLD = "\033[1m";
+    const char* Display::ITALIC = "\033[3m";
+    const char* Display::UNDERLINE = "\033[4m";
+    const char* Display::BLINK = "\033[5m";
+    const char* Display::VIDEO = "\033[7m";
+    const char* Display::CONCEAL = "\033[8m";
+    const char* Display::CROSSED = "\033[9m";
+
+    const char* Display::BLACK = "\033[30m";
     const char* Display::RED = "\033[31m";
     const char* Display::GREEN = "\033[32m";
     const char* Display::YELLOW = "\033[33m";
     const char* Display::BLUE = "\033[34m";
     const char* Display::MAGENTA = "\033[35m";
     const char* Display::CYAN = "\033[36m";
+    const char* Display::WHITE = "\033[37m";
+    const char* Display::DEFAULT = "\033[39m";
+
+    const char* Display::BBLACK = "\033[40m";
+    const char* Display::BRED = "\033[41m";
+    const char* Display::BGREEN = "\033[42m";
+    const char* Display::BYELLOW = "\033[43m";
+    const char* Display::BBLUE = "\033[44m";
+    const char* Display::BMAGENTA = "\033[45m";
+    const char* Display::BCYAN = "\033[46m";
+    const char* Display::BWHITE = "\033[47m";
+    const char* Display::BDEFAULT = "\033[49m";
 
     void Display::PrintBanner ( ) {
         std::cout << BOLD << YELLOW << R"(
